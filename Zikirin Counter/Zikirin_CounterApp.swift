bca9342fd476e2434c22a445695ebf2e50c4851b
hafizhMo <hafizh.hizb@gmail.com>
2023-09-17 22:35:03 +0700

    Initial Commit

---
+++ Zikirin Counter/Zikirin_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zikirin_CounterApp.swift
+//  Zikirin Counter
+//
+//  Created by Hafizh Mo on 17/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Zikirin_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
