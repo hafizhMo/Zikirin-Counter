1cb883d3e1ebeaa5cefb2a465cf4216e7ab3eb9a
hafizhMo <hafizh.hizb@gmail.com>
2023-09-18 22:13:49 +0700

    update UI PublicTotal

--- Zikirin Counter/Scenes/PublicTotalView.swift
+++ Zikirin Counter/Scenes/PublicTotalView.swift
@@ -11,19 +11,59 @@ struct PublicTotalView: View {
   @StateObject var viewModel = ViewModel()
   
   var body: some View {
-    VStack {
-      if viewModel.total != nil {
-        Text(viewModel.total!)
-          .bold()
-          .font(.largeTitle)
-      } else {
-        Text("Fetching value...")
-          .padding()
-          .background(.gray)
+    ZStack {
+      Color.gray.opacity(0.05).ignoresSafeArea()
+      VStack(spacing: 0) {
+        publicZikirCard()
+        otherCard()
+        Spacer()
       }
     }
     .onAppear {
       viewModel.observeTotal()
+    }
+  }
+  
+  private func publicZikirCard() -> some View {
+    ZStack {
+      Rectangle()
+        .foregroundColor(.white)
+        .frame(maxWidth: .infinity, maxHeight: 250)
+        .cornerRadius(20)
+        .shadow(color: Color.black.opacity(0.1), radius: 12, x: 1, y: 1)
+        .padding()
+      VStack(alignment: .leading) {
+        if viewModel.total != nil {
+          Text("Total Zikir Around the World".uppercased())
+            .bold()
+            .font(.caption)
+            .foregroundColor(.primary.opacity(0.5))
+          HStack(alignment: .firstTextBaseline) {
+            Text(viewModel.total!)
+              .font(.system(size: 72, weight: .heavy))
+            Text("X")
+              .font(.system(size: 32, weight: .black))
+            Spacer()
+          }
+        } else {
+          Text("Fetching value...")
+            .padding()
+            .background(.gray)
+        }
+      }
+      .padding()
+      .padding(.horizontal)
+    }
+  }
+  
+  private func otherCard() -> some View {
+    ZStack {
+      Rectangle()
+        .foregroundColor(.white)
+        .frame(maxWidth: .infinity)
+        .cornerRadius(20)
+        .shadow(color: Color.black.opacity(0.1), radius: 12, x: 1, y: 1)
+        .padding(.horizontal)
     }
   }
 }
